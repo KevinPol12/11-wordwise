e17b16ac700e810041947a7fa6c44d9ea2c8c77c
KevinPol12 <kevinpolanco12@gmail.com>
2024-04-23 14:54:28 -0600

    Implementing the PageNav using the <Link/> from react-router-dom

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
-function Homepage() {
-  return <div>WorldWise</div>;
-}
+import { Link } from "react-router-dom";
+import PageNav from "../Components/PageNav";
 
-export default Homepage;
+export default function Homepage() {
+  return (
+    <div>
+      {/*We could have added added this PageNav in the App at the routes level - 
+      but we are adding it here to each of the pages*/}
+      <PageNav />
+      <h1>WorldWise</h1>
+      <Link to="/product">Product</Link>
+    </div>
+  );
+}
