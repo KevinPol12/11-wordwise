fb2ec798e839d7aa4c1a4521d6b41195d4487ec1
KevinPol12 <kevinpolanco12@gmail.com>
2024-04-24 17:00:44 -0600

    Implementing the cities and countries lists

--- src/Components/AppNav.jsx
+++ src/Components/AppNav.jsx
@@ -6,10 +6,12 @@ function AppNav() {
     <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/app/cities">Cities</NavLink>
+          {/*No need to list the full path /app/cities - the route will
+          identify it is a nested route a move it appropriately*/}
+          <NavLink to="cities">Cities</NavLink>
         </li>
         <li>
-          <NavLink to="/app/countries">Countries</NavLink>
+          <NavLink to="countries">Countries</NavLink>
         </li>
       </ul>
     </nav>
---
+++ src/Components/CityItem.jsx
@@ -0,0 +1,25 @@
+import styles from "./CityItem.module.css";
+
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(new Date(date));
+
+  formatDate(date);
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <span className={styles.date}>({formatDate(date)})</span>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
+
+export default CityItem;
