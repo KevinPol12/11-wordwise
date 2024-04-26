2e36f2cc040d7d3c15beedb73bc1d2959d72f098
KevinPol12 <kevinpolanco12@gmail.com>
2024-04-26 16:00:51 -0600

    Setting up our first leaflet map in a REACT project

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import Button from "./Button";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -11,20 +12,26 @@ function Map() {
   const lng = searchParams.get("lng");
 
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  /*This navigate will add to the URL whatever we pass as parameter to their 
-    parent route*/
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
-      {/*Besides it, we get a setter function which allow us to update the 
-      URL params */}
-      <button onClick={() => setSearchParams({ lat: 50, lng: 95 })}>
-        Change Pos
-      </button>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
